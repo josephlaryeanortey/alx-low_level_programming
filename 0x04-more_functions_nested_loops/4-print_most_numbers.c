8580592b27fceae56f38d79d0eb68969dd3f4f73
josephlaryeanortey <christiangospelhype@gmail.com>
2023-09-15 15:40:56 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers from 0 to 9, excluding 2 and 4, followed by a new line
+ * print_most_numbers - Prints the numbers from 0 to 9, excluding 2 and 4,
+ *                     followed by a new line
  */
 void print_most_numbers(void)
 {
-    char digit = '0';
+	char digit = '0';
 
-    while (digit <= '9')
-    {
-        if (digit != '2' && digit != '4')
-        {
-            _putchar(digit);
-        }
-        digit++;
-    }
+	while (digit <= '9')
+	{
+		if (digit != '2' && digit != '4')
+		{
+			_putchar(digit);
+		}
+		digit++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
