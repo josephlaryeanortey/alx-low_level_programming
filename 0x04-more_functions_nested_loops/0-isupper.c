6bd1e7dafd1be1e57f8f61dfa1cd5e2f076ea3d6
josephlaryeanortey <christiangospelhype@gmail.com>
2023-09-14 20:03:37 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,12 @@
 #include "main.h"
+
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
-* Return: Always 0.
-*/
+ * _isupper - Checks if a character is uppercase.
+ * @c: The character to check.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
 int _isupper(int c)
 {
-
-
- if (c >= 'A' && c <= 'Z')
- {
- return (1);
- }
- else
-{
- return (0);
-}
-
+    return (c >= 'A' && c <= 'Z');
 }
