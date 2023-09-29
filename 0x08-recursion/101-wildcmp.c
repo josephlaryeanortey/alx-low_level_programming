4e7783dfa8bd5f78bee76bc568d87217424c83a8
josephlaryeanortey <christiangospelhype@gmail.com>
2023-09-28 17:21:16 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,20 +9,20 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	/* If we reach the end of both strings, they are identical */
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
+    /* If we reach the end of both strings, they are identical */
+    if (*s1 == '\0' && *s2 == '\0')
+        return 1;
 
-	/* If the current characters match or s2 has '*', move to the next characters */
-	if (*s1 == *s2 || *s2 == '*')
-		return (wildcmp(s1 + 1, s2 + 1));
+    /* If the current characters match or s2 has '*', move to the next characters */
+    if (*s1 == *s2 || *s2 == '*')
+        return wildcmp(s1 + 1, s2 + 1);
 
-	/* If s2 has '*', either skip it or use it to match one or more characters in s1 */
-	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-		return (0);
-	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+    /* If s2 has '*', either skip it or use it to match one or more characters in s1 */
+    if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+        return 0;
+    if (*s2 == '*')
+        return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
 
-	/* If the characters don't match and s2 doesn't have '*', strings are not identical */
-	return (0);
+    /* If the characters don't match and s2 doesn't have '*', strings are not identical */
+    return 0;
 }
