594f159d4f0364909aa254f15c72d55cdaa34b91
josephlaryeanortey <christiangospelhype@gmail.com>
2023-12-07 13:25:25 -0800

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,23 +1,14 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in
- * a double linked list
+ * dlistint_len - Returns the number of elements in a linked dlistint_t list.
+ * @h: A pointer to the head of the dlistint_t list.
  *
- * @h: head of the list
- * Return: the number of nodes
+ * Return: The number of elements in the list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    int count;
-
-    count = 0;
-
-    if (h == NULL)
-        return (count);
-
-    while (h->prev != NULL)
-        h = h->prev;
+    size_t count = 0;
 
     while (h != NULL)
     {
@@ -25,5 +16,5 @@ size_t dlistint_len(const dlistint_t *h)
         h = h->next;
     }
 
-    return (count);
+    return count;
 }
