a67f2e8c4fd1b3952c93ce70b5a379b5fd15018b
josephlaryeanortey <christiangospelhype@gmail.com>
2023-12-11 17:12:52 -0800

    Create a script that creates a dynamic library called liball.so from all the .c files that are in the current directory

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-gcc -c -fpic *.c
-gcc -shared -o liball.so *.o
+
+# Compile each .c file into position-independent code (-fPIC)
+for file in *.c; do
+    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c -fPIC "$file"
+done
+
+# Create the dynamic library liball.so from the necessary object files
+gcc -shared -o liball.so 0-main.o 0-putchar.o 100-atoi.o 1-isdigit.o 1-strncat.o 2-strlen.o 3-islower.o 3-strcmp.o 4-isalpha.o 5-strstr.o 9-strcpy.o
+
+# Clean up: remove the temporary object files
+rm *.o
